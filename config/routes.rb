0b911cf02cc09b2599d820481a158936847d681d
JLisaW <j.lisa.wang@gmail.com>
2017-05-04 16:24:14 -0400

    initial deployment commit

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   post '/sign-in' => 'users#signin'
   delete '/sign-out/:id' => 'users#signout'
   patch '/change-password/:id' => 'users#changepw'
+  get '/reminders' => 'reminders#index'
   resources :users, only: [:index, :show]
+  resources :reminders, only: [:index, :show, :destroy]
 end
