dcbd60f13d18f3e8c407a7d907d0728e935889a2
JLisaW <j.lisa.wang@gmail.com>
2017-05-09 19:19:56 -0400

    update button functioning

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ProtectedController
   before_action :set_task, only: [:show, :update, :destroy]
-  # before_action :validate_user, only: [:index, :create, :update, :set_task]
+  before_action :validate_user
 
   # GET /tasks
   def index
     # @tasks = Task.all
-    @tasks = current_user.tasks
+    @tasks = current_user.tasks.all
 
     render json: @tasks
   end
@@ -30,6 +30,7 @@ class TasksController < ProtectedController
 
   # PATCH/PUT /tasks/1
   def update
+    binding.pry
     if @task.update(task_params)
       render json: @task
     else
@@ -47,8 +48,12 @@ class TasksController < ProtectedController
   # Use callbacks to share common setup or constraints between actions.
 
   def set_task
-  # @task = current_user.items.find(params[:id])
-  @task = Task.find(params[:id])
+  @task = current_user.tasks.find(params[:id])
+  # @task = Task.find(params[:id])
+  end
+
+  def validate_user
+    set_current_user
   end
 
   # Only allow a trusted parameter "white list" through.
